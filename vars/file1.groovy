3fc96b95894b60c2398bd77bdac31709f44a1e63
Ubuntu <ubuntu@NikithaJenkins.fwcnccvptddejchq1mljlyvkzh.gx.internal.cloudapp.net>
2019-05-26 04:38:37 +0000

    second

--- vars/file1.groovy
+++ vars/file1.groovy
@@ -64,7 +64,7 @@ stage ('Application Deployment'){
 }
 }
 catch (err) { 
-          mail body:"${err}. Check result at ${BUILD_URL}", subject: "Build Failed ${PROJECT_NAME} - Build # ${BUILD_NUMBER}", to: params.email
+          mail body:"${err}. Check result at ${BUILD_URL}", subject: "Build Failed ${JOB_NAME} - Build # ${BUILD_NUMBER}", to: params.email
           currentBuild.result = 'FAILURE'
       }
 }
